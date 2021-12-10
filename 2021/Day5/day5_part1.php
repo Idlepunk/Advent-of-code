f6ea9ddd6e008b7bd4bdc0531f105c7bd9fd45ad
Asher <asher@mpksystems.com>
2021-12-11 01:06:21 +1100

    add day 5

---
+++ 2021/Day5/day5_part1.php
@@ -0,0 +1,83 @@
+<?php
+$input = file_get_contents("input");
+
+$ventCoordinates = getVentCoordinatesFromInput($input);
+$grid = buildGrid();
+$grid = populateGridWithVents($grid, $ventCoordinates);
+$points = calculateNumberOfOverlappingVents($grid);
+echo $points;
+
+function getVentCoordinatesFromInput($input) {
+    $lines = explode("\n", $input);
+    $positions = [];
+    foreach ($lines as $line) {
+        $posLines = explode(" -> ", $line);
+        $start = explode(",", $posLines[0]);
+        $coordinate["start"]["x"] = (int)$start[0];
+        $coordinate["start"]["y"] = (int)$start[1];
+
+        $end = explode(",", $posLines[1]);
+        $coordinate["end"]["x"] = (int)$end[0];
+        $coordinate["end"]["y"] = (int)$end[1];
+        $positions[] = $coordinate;
+    }
+    return $positions;
+}
+
+function buildGrid() {
+    $width = 1000;
+    $height = 1000;
+    $grid = [];
+    for ($x = 0; $x < $width; $x++) {
+        for ($y = 0; $y < $height; $y++) {
+            $grid[$x][$y] = 0;
+        }
+    }
+    return $grid;
+}
+
+function populateGridWithVents(array $grid, $vents) {
+    foreach ($vents as $vent) {
+        $start = $vent["start"];
+        $end = $vent["end"];
+
+        if ($start["x"] === $end["x"]) {
+            if ($start["y"] > $end["y"]) {
+                for ($y = $end["y"]; $y <= $start["y"]; $y++) {
+                    addVentToCoordinate($grid, $start["x"], $y);
+                }
+            } else {
+                for ($y = $start["y"]; $y <= $end["y"]; $y++) {
+                    addVentToCoordinate($grid, $start["x"], $y);
+                }
+            }
+        } else if ($start["y"] === $end["y"]) {
+            if ($start["x"] > $end["x"]) {
+                for ($x = $end["x"]; $x <= $start["x"]; $x++) {
+                    addVentToCoordinate($grid, $x, $start["y"]);
+                }
+            } else {
+                for ($x = $start["x"]; $x <= $end["x"]; $x++) {
+                    addVentToCoordinate($grid, $x, $start["y"]);
+                }
+            }
+        }
+    }
+    return $grid;
+}
+
+function addVentToCoordinate(array &$grid, $x, $y) {
+    $grid[$x][$y]++;
+}
+
+function calculateNumberOfOverlappingVents(array $grid) {
+    $points = 0;
+    foreach ($grid as $x) {
+        foreach ($x as $y) {
+            if ($y >= 2) {
+                $points++;
+            }
+        }
+    }
+    return $points;
+}
