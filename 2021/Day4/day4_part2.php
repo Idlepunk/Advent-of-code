02644ea01e206be5b3157156a853d7071f5cdcbb
Asher <asher@mpksystems.com>
2021-12-10 18:36:05 +1100

    add day 4

---
+++ 2021/Day4/day4_part2.php
@@ -0,0 +1,117 @@
+<?php
+$input = file_get_contents("input");
+$input = str_replace("  ", " ", $input);
+$input = str_replace(" ", ",", $input);
+
+$drawNumbers = getDrawNumbers($input);
+$boards = getBoards($input);
+
+$wins = [];
+$boardCount = count($boards);
+
+foreach ($drawNumbers as $drawNumber) {
+    for ($i = 0; $i < count($boards); $i++) {
+        $board = &$boards[$i];
+        $board = markNumberOnBoard($drawNumber, $board);
+        if (checkBoardForWin($board) && !in_array($i, $wins)) {
+            $wins[] = $i;
+        }
+        if (count($wins) === $boardCount) {
+            $score = calculateScore($board, $drawNumber);
+            echo $score;
+            die;
+        }
+    }
+}
+
+function calculateScore($board, $drawNumber) {
+    $cellSum = 0;
+    foreach ($board as $line) {
+        foreach ($line as $cell) {
+            if (!$cell["marked"]) {
+                $cellSum += $cell["value"];
+            }
+        }
+    }
+    return $cellSum * $drawNumber;
+}
+
+function getBoards($input) {
+    $boardStrings = explode("\n\n", $input);
+    array_shift($boardStrings);
+    $boards = [];
+    foreach ($boardStrings as $boardString) {
+        $csv = str_replace(",\n", "\n", $boardString);
+        $csv = str_replace("\n,", "\n", $csv);
+        $csv = ltrim($csv,',') ;
+        $lines = explode("\n", $csv);
+        foreach ($lines as &$line) {
+            $line = explode(",", $line);
+        }
+        $board = [];
+
+        unset($line);
+        foreach ($lines as $line) {
+            $boardLine = [];
+            foreach ($line as $cellValue) {
+                $cell = [];
+                $cell["value"] = $cellValue;
+                $cell["marked"] = false;
+                $boardLine[] = $cell;
+            }
+            $board[] = $boardLine;
+        }
+        $boards[] = $board;
+    }
+    return $boards;
+}
+
+function getDrawNumbers($input) {
+    $drawNumbersString = strtok($input, "\n\n");
+    return explode(",", $drawNumbersString);
+}
+
+function checkBoardForWin($board) {
+    return checkForHorizontalWin($board) || checkForVerticalWin($board);
+}
+
+function checkForVerticalWin($board) {
+    for ($x = 0; $x < count($board); $x++) {
+        $line = $board[$x];
+        $allMarked = true;
+        for ($y = 0; $y < count($line); $y++) {
+            if (!$board[$y][$x]["marked"]) {
+                $allMarked = false;
+            }
+        }
+        if ($allMarked) {
+            return true;
+        }
+    }
+}
+
+function checkForHorizontalWin($board) {
+    foreach ($board as $line) {
+        $allMarked = true;
+        foreach ($line as $cell) {
+            if (!$cell["marked"]) {
+                $allMarked = false;
+            }
+        }
+        if ($allMarked) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function markNumberOnBoard($drawNumber, $board) {
+    foreach ($board as &$line) {
+        foreach ($line as &$cell) {
+            if ($cell["value"] === $drawNumber) {
+                $cell["marked"] = true;
+            }
+        }
+    }
+    return $board;
+}
